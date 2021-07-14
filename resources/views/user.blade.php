2211ab8871af0b446e2f12f838336aa2290ff813
cmo7 <marcelinocb@gmail.com>
2021-07-14 22:54:57 +0100

    timestamp para los usuarios

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,7 +1,8 @@
 @extends('layout')
 @section('content')
     <h1>{{ $user->name }}</h1>
-    <a href="mailto:{{ $user->email }}"></a>
+    <a href="mailto:{{ $user->email }}">email</a>
+    Miembro desde: {{$user->created_at->locale('es')->diffForHumans()}}
     @foreach ($user->posts as $post)
         <article>
             <h2><a href="/post/{{ $post->slug }}">{{ $post->title }}</a></h2>
