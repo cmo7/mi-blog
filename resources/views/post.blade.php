cd021e829eb172d4e98978fbfc4ec09fbda2369a
cmo7 <marcelinocb@gmail.com>
2021-07-17 15:50:47 +0100

    añadida plantilla con CSS y añadidos componentes de blade

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,16 +1,20 @@
-@extends ('layout')
-@section('content')
+<x-layout>
 
-    <article>
-        <h2>{{ $post->title }}</h2>
-        <div class="info">
-            Escrito por <a href="/user/{{ $post->user->id }}">{{ $post->user->name }}</a>
-            en <a href="{{ '/category/' . $post->category->slug }}">{{ $post->category->name }}</a>
-        </div>
-        <div>
-            {{ $post->content }}
-        </div>
-    </article>
-    <a href="/">Volver al inicio</a>
+    <x-interior-header />
 
-@endsection
+    <div id="main">
+        <div class="inner">
+            <article>
+                <h1>{{ $post->title }}</h1>
+                <span class="image main"><img src="/images/{{ $post->image }}" alt="" /></span>
+                <div class="info">
+                    Escrito por <a href="/user/{{ $post->user->id }}">{{ $post->user->name }}</a>
+                    en <a href="{{ '/category/' . $post->category->slug }}">{{ $post->category->name }}</a>
+                </div>
+                <div>
+                    {!! $post->content !!}
+                </div>
+            </article>
+        </div>
+    </div>
+</x-layout>
